ff778eccd97ffc84d192860291401b58e3d43b2a
mtsalenc <mtsalenc@gmail.com>
2020-09-22 12:01:42 -0300

    test: setup failing tests

---
+++ contracts/Deps.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.5.11;
+
+import { SimpleCentralizedArbitrator } from "@kleros/erc-792/contracts/examples/SimpleCentralizedArbitrator.sol";
+import { GeneralizedTCR } from "@kleros/tcr/contracts/GeneralizedTCR.sol";
---
+++ test/gtcr.js
@@ -0,0 +1,172 @@
+
+/* global describe, it, before, step */
+const Web3 = require('web3')
+const TruffleContract = require('@truffle/contract')
+const { default: axios } = require('axios')
+const delay = require('delay')
+const fs = require('fs-extra')
+const path = require('path')
+const { gtcrEncode, ItemTypes } = require('@kleros/gtcr-encoder')
+const { expect } = require('chai')
+const { promisify } = require('util')
+
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+const web3 = new Web3(provider)
+
+function getContract (contractName) {
+  const C = TruffleContract(fs.readJsonSync(path.join(
+    __dirname, '..', 'build', 'contracts', `${contractName}.json`
+  )))
+  C.setProvider(provider)
+  return C
+}
+
+const SimpleCentralizedArbitrator = getContract('SimpleCentralizedArbitrator')
+const GeneralizedTCR = getContract('GeneralizedTCR')
+
+async function queryGraph (query) {
+  return (await axios.post('http://localhost:8000/subgraphs', { query })).data.data
+}
+
+const subgraphName = 'kleros/gtcr'
+
+async function querySubgraph (query) {
+  return (await axios.post(`http://localhost:8000/subgraphs/name/${subgraphName}`, { query })).data.data
+}
+
+async function waitForGraphSync (targetBlockNumber) {
+  if (targetBlockNumber == null) { targetBlockNumber = await web3.eth.getBlockNumber() }
+
+  while (true) {
+    await delay(100)
+    const {
+      subgraphs: [{
+        currentVersion: {
+          id: currentVersionId,
+          deployment: {
+            latestEthereumBlockNumber
+          }
+        },
+        versions: [{ id: latestVersionId }]
+      }]
+    } = await queryGraph(`{
+      subgraphs(
+        where: {name: "${subgraphName}"}
+        first: 1
+      ) {
+        currentVersion {
+          id
+          deployment {
+            latestEthereumBlockNumber
+          }
+        }
+        versions(
+          orderBy: createdAt,
+          orderDirection: desc,
+          first: 1
+        ) {
+          id
+        }
+      }
+    }`)
+
+    if (
+      currentVersionId === latestVersionId &&
+      latestEthereumBlockNumber === targetBlockNumber
+    ) { break }
+  }
+}
+
+/** Increases ganache time by the passed duration in seconds and mines a block.
+ * @param {number} duration time in seconds
+ */
+async function increaseTime (duration) {
+  await promisify(web3.currentProvider.send.bind(web3.currentProvider))({
+    jsonrpc: '2.0',
+    method: 'evm_increaseTime',
+    params: [duration],
+    id: new Date().getTime()
+  })
+
+  await advanceBlock()
+}
+
+/** Advance to next mined block using `evm_mine`
+ * @returns {promise} Promise that block is mined
+ */
+function advanceBlock () {
+  return promisify(web3.currentProvider.send.bind(web3.currentProvider))({
+    jsonrpc: '2.0',
+    method: 'evm_mine',
+    id: new Date().getTime()
+  })
+}
+
+describe('GTCR subgraph', function () {
+  let centralizedArbitrator
+  let gtcr
+
+  let submitter
+  before('get deployed contracts and accouts', async function () {
+    centralizedArbitrator = await SimpleCentralizedArbitrator.deployed()
+    gtcr = await GeneralizedTCR.deployed()
+
+    const accounts = await web3.eth.getAccounts()
+    submitter = accounts[0]
+  })
+
+  it('exists', async function () {
+    const { subgraphs } = await queryGraph(`{
+      subgraphs(first: 1, where: {name: "${subgraphName}"}) {
+        id
+      }
+    }`)
+
+    subgraphs.should.be.not.empty()
+  })
+
+  step('Submit item', async function () {
+    const columns = [
+      {
+        label: 'Name',
+        type: ItemTypes.TEXT
+      },
+      {
+        label: 'Ticker',
+        type: ItemTypes.TEXT
+      }
+    ] // This information can be found in the TCR meta evidence.
+    const tokenData = {
+      Name: 'Pinakion',
+      Ticker: 'PNK'
+    }
+
+    const [, REGISTERED, REGISTRATION_REQUESTED] = [0, 1, 2, 3, 4]
+
+    const arbitrationCost = await centralizedArbitrator.arbitrationCost('0x00')
+    const encodedData = gtcrEncode({ columns, values: tokenData })
+    await gtcr.addItem(encodedData, { from: submitter, value: arbitrationCost })
+    const itemID = await gtcr.itemList(0)
+
+    await advanceBlock()
+    await waitForGraphSync()
+    expect((await querySubgraph(`{
+      gtcr(id: "${itemID}") {
+        data
+        status
+      }
+    }`)).gtcr).to.deep.equal({ data: encodedData, status: REGISTRATION_REQUESTED })
+
+    await increaseTime(10)
+    await gtcr.executeRequest(itemID, { from: submitter })
+
+    await advanceBlock()
+    await waitForGraphSync()
+    expect((await querySubgraph(`{
+      gtcr(id: "${itemID}") {
+        data
+        status
+      }
+    }`)).gtcr).to.deep.equal({ data: encodedData, status: REGISTERED })
+  })
+})
