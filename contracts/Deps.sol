50a90e6e63c16210711359b07cf1dd3b36a16a64
mtsalenc <matheus@kleros.io>
2021-07-16 13:13:41 +0100

    feat: add testnets and remove deprecated code

--- contracts/Deps.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-import { TestArbitrator } from "./TestArbitrator.sol";
-import { GTCRFactory } from "@kleros/tcr/contracts/GTCRFactory.sol";
-import { GeneralizedTCR } from "@kleros/tcr/contracts/GeneralizedTCR.sol";
-import { IArbitrator } from "@kleros/erc-792/contracts/IArbitrator.sol";
--- render-templates.js
+++ render-templates.js
@@ -1,45 +1,37 @@
 const fs = require('fs-extra');
 const mustache = require('mustache');
 
-module.exports = function(callback) {
-  (async () => {
-    const networkType = await web3.eth.net.getNetworkType();
-    const templateData = {
-      network: {
-        main: 'mainnet',
-        private: 'development',
-      }[networkType] || networkType
-    };
+const chainNameToChainId = {
+  xdai: 100,
+  mainnet: 1,
+  kovan: 42,
+  rinkeby: 4
+}
 
-    for(const contractName of [
-      'GTCRFactory',
-      'IArbitrator',
-      'GeneralizedTCR'
-    ]) {
-      const { abi } = fs.readJsonSync(`build/contracts/${contractName}.json`);
-      fs.outputJsonSync(`abis/${contractName}.json`, abi, { spaces: 2 });
+async function main() {        
+  const networkName = process.argv[2]
+  const chainId = chainNameToChainId[networkName]
+  const deployments = JSON.parse(fs.readFileSync('networks.json', 'utf8'));  
+  const { address, startBlock } = deployments['GTCRFactory'][chainId]
+  const templateData = {
+    network: networkName
+  };
+  templateData['GTCRFactory'] = {
+    address,
+    addressLowerCase: address.toLowerCase(),
+    startBlock,
+  };   
 
-      const C = artifacts.require(contractName);
-      try {
-        const { address } = C;
-        templateData[contractName] = {
-          address,
-          addressLowerCase: address.toLowerCase(),
-          startBlock: (
-            await web3.eth.getTransactionReceipt(C.transactionHash)
-          ).blockNumber,
-        };
-      } catch (e) {}
-    }
+  for (const templatedFileDesc of [
+    ['subgraph', 'yaml']
+  ]) {
+    const template = fs.readFileSync(`${templatedFileDesc[0]}.template.${templatedFileDesc[1]}`).toString();
+    fs.writeFileSync(
+      `${templatedFileDesc[0]}.${templatedFileDesc[1]}`,
+      mustache.render(template, templateData),
+    );
+  }
+}
 
-    for (const templatedFileDesc of [
-      ['subgraph', 'yaml']
-    ]) {
-      const template = fs.readFileSync(`${templatedFileDesc[0]}.template.${templatedFileDesc[1]}`).toString();
-      fs.writeFileSync(
-        `${templatedFileDesc[0]}.${templatedFileDesc[1]}`,
-        mustache.render(template, templateData),
-      );
-    }
-  })().then(() => callback(), callback);
-};
+main()
+
