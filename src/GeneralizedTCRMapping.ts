5546d54be749128013f54b838d15599e7e811df8
devofficer <nikolaytoplev@gmail.com>
2022-06-09 06:23:31 +1000

    fix: adding tcr address to meta evidence entity

--- src/GeneralizedTCRMapping.ts
+++ src/GeneralizedTCRMapping.ts
@@ -412,6 +412,7 @@ export function handleMetaEvidence(event: MetaEvidenceEvent): void {
 
   metaEvidence.URI = event.params._evidence;
   metaEvidence.timestamp = event.block.timestamp;
+  metaEvidence.tcrAddress = event.address;
   metaEvidence.save();
 
   if (
